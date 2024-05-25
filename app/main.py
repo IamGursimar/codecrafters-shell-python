85f21d4fb02ffee4212a26e24f2676b4a37d4ac4
IamGursimar <gursimarsingh195@gmail.com>
2024-05-25 16:57:24 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,9 +5,10 @@ def main():
 
     sys.stdout.write("$ ")
     sys.stdout.flush()
-
     # Wait for user input
-    input()
+    argument = input()
+    sys.stderr.write(f"{argument}: command not found\n")
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
