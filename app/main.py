9df770c4c655f21104206e000ebc75a52c755694
IamGursimar <gursimarsingh195@gmail.com>
2024-05-27 15:54:25 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import sys
 
 
 def handle_input(user_argument):
+    if user_argument == "exit 0":
+        return False
     sys.stderr.write(f"{user_argument}: command not found\n")
     sys.stdout.flush()
 
@@ -13,7 +15,9 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_argument = input()
-        handle_input(user_argument)
+        handle_response = handle_input(user_argument)
+        if handle_response == False:
+            break
 
 
 if __name__ == "__main__":
