69c526cdca04a379631dc7e155da29ec84374caa
IamGursimar <gursimarsingh195@gmail.com>
2024-05-27 21:10:03 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,32 +2,33 @@ import os
 import sys
 
 builtin_commands = ["exit", "echo", "type"]
-
 def handle_input(user_argument):
-    if "type" == user_argument.split(" ")[0]:
-        if user_argument.split(" ")[1] in builtin_commands:
-            sys.stdout.write(f"{user_argument.split(" ")[1]} is a shell builtin\n")
+    command_initial = user_argument.split(" ")[0]
+    if len(user_argument.split(" ")) > 1:
+        command_argument = user_argument.split(" ")[1]
+    if "type" == command_initial:
+        if command_argument in builtin_commands:
+            sys.stdout.write(f"{command_argument} is a shell builtin\n")
         else:
             paths = os.getenv("PATH").split(":")
             for path in paths:
-                if os.path.exists(f"{path}/{user_argument.split(" ")[1]}"):
-                    sys.stdout.write(f"{user_argument.split(" ")[1]} is {path}/{user_argument.split(" ")[1]}\n")
+                if os.path.exists(f"{path}/{command_argument}"):
+                    sys.stdout.write(f"{command_argument} is {path}/{command_argument}\n")
                     break
             else:
-                sys.stdout.write(f"{user_argument.split(" ")[1]} not found\n")
-
+                sys.stdout.write(f"{command_argument} not found\n")
     elif user_argument == "exit 0":
         return False
-    elif "echo" == user_argument.split(" ")[0]:
+    elif "echo" == command_initial:
         sys.stdout.write(f"{user_argument.replace("echo", "")}\n")
+    elif os.path.exists(f"{command_initial}"):
+        sys.stdout.write(os.popen(user_argument).read())        
     else:
         sys.stderr.write(f"{user_argument}: command not found\n")
         sys.stdout.flush()
 
 
 def main():
-
-    # Wait for user input
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -35,7 +36,6 @@ def main():
         handle_response = handle_input(user_argument)
         if handle_response == False:
             break
-
 
 if __name__ == "__main__":
     main()
